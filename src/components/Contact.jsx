a2c628a77bfe0f482e06827d0343a3c358982be4
Ilham-Syed <ilhamsyed1310@gmail.com>
2024-05-18 00:26:07 +0530

    Contact Component updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import { CONTACT } from "../constants";
 
 const Contact = () => {
   return (
-    <div className="border-b border-neutral-900 h-[400px]">
+    <div className="h-[400px]">
       <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
       <div className="text-center tracking-tighter">
         <p className="my-4">{CONTACT.address}</p>
