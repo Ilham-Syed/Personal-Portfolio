1e62d6bc6ae67c4db8ada1d17b09139e1cbc8744
Ilham-Syed <ilhamsyed1310@gmail.com>
2024-05-26 15:06:43 +0530

    Resume link updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -50,7 +50,7 @@ const Hero = () => {
               variants={container(1.5)}
               initial="hidden"
               animate="visible"
-              href="https://drive.google.com/file/d/1yDsBClSZFcFGSPqnoxbb2-7RBrJ0RtRh/view"
+              href="https://drive.google.com/file/d/1S8SIlCtrQ34HqwVrrUg2rf6Qriet0o96/view"
               target="_blank"
               rel="noopener noreferrer"
               className="bg-transparent text-grey-500 font-bold py-2 px-1 rounded border-b transition duration-300 hover:shadow-lg hover:bg-gray-900 sm:text-sm sm:mb-2"
